84bdf681a0f79c591aa82b5deb088f3863537ccf
Ernest Korir <ernestbore3@gmail.com>
2023-03-31 01:12:04 +0300

    fix linters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import Books from './components/books';
 import './App.css';
 import Categories from './components/categories';
@@ -7,13 +7,13 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-   <>
-   <Navbar />
-   <Routes>
-      <Route path="/" element={<Books title="React in Action" author="Mark Tielens" />} />
-      <Route path="/categories" element={<Categories />} />
-   </Routes>
-   </>
+    <>
+    <Navbar />
+    <Routes>
+       <Route path="/" element={<Books title="React in Action" author="Mark Tielens" />} />
+       <Route path="/categories" element={<Categories />} />
+    </Routes>
+    </>
   );
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,11 @@
 const Form = () => (
-    <div>
+  <div>
       <form className="Form flexColumn">
         <input type="text" id="author" placeholder="Author" />
         <input type="text" id="title" placeholder="Book Title" />
         <button type="submit">Submit</button>
       </form>
     </div>
-  );
+);
   
-  export default Form;
+export default Form;
