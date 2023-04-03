b5c881396d00fbbf25c025503073b8308e2356b7
Ernest Korir <ernestbore3@gmail.com>
2023-04-03 15:08:10 +0300

    Add bookSlice

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}></Provider>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,12 @@
+import { createAction, createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    book : [],
+}
+
+export const bookSlice = createSlice({
+    name: 'book',
+    initialState,
+    reducers: {
+        
+    }
+})
