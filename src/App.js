1e8798736f45693ae5647dba45bad70754239b11
Ernest Korir <ernestbore3@gmail.com>
2023-04-04 16:53:47 +0300

    Add categoriespage.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Books from './components/books';
+import Books from './pages/bookpage';
 import './App.css';
-import Categories from './components/categories';
+import Categories from './pages/categories';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Books title="React in Action" author="Mark Tielens" />} />
+        <Route path="/" element={<Books />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
     </>
