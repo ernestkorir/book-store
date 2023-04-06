51fcf5f4e93deaf5d37cc52d755368f40bf3ce1d
Ernest Korir <ernestbore3@gmail.com>
2023-04-06 16:38:46 +0300

    Fix style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Books</Link>
-      </li>
-      <div className="verticalLine" />
-      <li>
-        <Link to="/categories">Categories</Link>
-      </li>
-    </ul>
+  <nav className="panel-bg">
+    <div className="firstpart">
+      <div>
+        <span className="Bookstore-CMS Text-Style-3">Bookstore CMS</span>
+      </div>
+
+      <div className="BOOKS">
+        <Link to="/" className="navlink">
+          BOOKS
+        </Link>
+      </div>
+
+      <div className="categories">
+        <Link to="/categories" className="navlink">
+          CATEGORIES
+        </Link>
+      </div>
+    </div>
+    <div className="user">
+      <i className="fa-solid fa-user" />
+    </div>
   </nav>
 );
 
